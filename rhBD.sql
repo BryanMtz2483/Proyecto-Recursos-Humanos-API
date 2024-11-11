e5c56853334632b29f67b6f4aba3400d5975e2c3
Bryan <mhzb3072@gmail.com>
2024-11-10 20:17:45 -0600

    Base de datos corregida

--- rhBD.sql
+++ rhBD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 10-11-2024 a las 01:15:53
+-- Tiempo de generación: 11-11-2024 a las 02:16:25
 -- Versión del servidor: 8.0.40
 -- Versión de PHP: 8.3.12
 
@@ -43,6 +43,30 @@ CREATE TABLE `employees` (
 INSERT INTO `employees` (`id`, `name`, `last_name`, `phone`, `email`, `address`) VALUES
 (1, 'Bryan', 'Martinez Tinoco', 4425244760, 'mhzb3072@gmail.com', 'Queretaro 2000');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_mail` varchar(50) NOT NULL,
+  `user_password` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `user_name`, `user_mail`, `user_password`) VALUES
+(1, 'Bryan2483', 'mhzb3072@gmail.com', '12345678'),
+(2, 'Bryan Giovanny 2', 'bgmt@gmail.com', '12345678'),
+(4, 'Bryan24832', 'mdndnfd', '12345678'),
+(5, 'Bryan Giovanny 3', 'bgmt@gmail.comm', '12345678'),
+(6, 'era', 'era@suputamadre.com', '12345678');
+
 --
 -- Índices para tablas volcadas
 --
@@ -56,6 +80,14 @@ ALTER TABLE `employees`
   ADD UNIQUE KEY `phone` (`phone`);
 
 --
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_mail` (`user_mail`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -64,6 +96,12 @@ ALTER TABLE `employees`
 --
 ALTER TABLE `employees`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
